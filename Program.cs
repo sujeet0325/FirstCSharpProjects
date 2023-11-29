eb2bc64333ac7db96f1c7c3e3fe9e0702ab347f6
sujeet0325 <142496120+sujeet0325@users.noreply.github.com>
2023-11-29 15:56:21 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpProjects
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a=10, b=20, c;
+            c = a + b;
+            Console.WriteLine("Addition = "+c);
+        }
+        public void Mul()
+        {
+            int a = 10, b = 20, c;
+            c = a * b;
+            Console.WriteLine("Multiplication = " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Mul();
+            p.Add();
+            Console.ReadLine();
+        }
+    }
+}
